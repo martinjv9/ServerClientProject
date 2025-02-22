cb817893f018248f045e01aa807912ede2a20ef0
Jimenez <martin.jimenez.243@my.csun.edu>
2025-02-21 18:16:24 -0800

    First Commit

---
+++ ServerProject/StinkyModule/City.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace StinkyModule;
+
+[Table("City")]
+public partial class City
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string Name { get; set; } = null!;
+
+    [Column("population")]
+    public int Population { get; set; }
+
+    [Column("countryID")]
+    public int CountryId { get; set; }
+
+    [ForeignKey("CountryId")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
