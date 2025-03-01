edd3c1a0568b80ca2235661df96d511252337368
Jimenez <martin.jimenez.243@my.csun.edu>
2025-02-28 18:10:46 -0800

    Commit 1

---
+++ ServerProject/ServerProject/Controllers/SeedController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StinkyModule;
+
+namespace ServerProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeedController(WorldCitiesSourceContext context) : ControllerBase
+    {
+        [HttpPost("Countries")]
+        public async Task<ActionResult> ImportCountriesAsync()
+        {
+
+        }
+
+        [HttpPost("Cities")]
+        public async Task<ActionResult> ImportCitiesAsync()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ ServerProject/ServerProject/Dtos/CityCountry.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServerProject.Dtos
+{
+    public class CityCountry
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public int Population { get; set; }
+
+        public string CountryName { get; set; } = null!;
+
+    }
+}
