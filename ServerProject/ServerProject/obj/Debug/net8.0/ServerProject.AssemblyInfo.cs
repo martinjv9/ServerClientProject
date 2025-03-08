f674941d887fe071ae1d5f20225c7b656580e9be
Jimenez <martin.jimenez.243@my.csun.edu>
2025-03-07 16:47:55 -0800

    Mid Update, added migration

--- ServerProject/ServerProject/obj/Debug/net8.0/ServerProject.AssemblyInfo.cs
+++ ServerProject/ServerProject/obj/Debug/net8.0/ServerProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb817893f018248f045e01aa807912ede2a20ef0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f05a60e4e92080d651208dc1a9066812e3fba458")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
